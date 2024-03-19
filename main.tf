ee68fd859ba662352bfc482a2223142968ad32f1
rachit89 <115970922+rachit89@users.noreply.github.com>
2024-03-19 15:45:31 +0530

    Version Upgrade
    1. Upgraded sonarqube version from 1.0.30 to 10.4.0

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ resource "kubernetes_namespace" "sonarqube" {
 resource "helm_release" "sonarqube" {
   depends_on = [kubernetes_namespace.sonarqube]
   name       = "sonarqube"
-  chart      = "sonarqube-lts"
+  chart      = "sonarqube"
   timeout    = 600
-  version    = var.chart_version
+  version    = var.helm_chart_version
   namespace  = var.namespace
   repository = "https://sonarsource.github.io/helm-chart-sonarqube"
   values = [
