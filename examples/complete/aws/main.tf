2eaa1782cd213cdfe3b4539b48aa863cf9ddb6e6
rachit89 <rachit.maheshwari@squareops.com>
2024-03-22 09:43:35 +0530

    Upgraded version of sonarqube helm chart

--- examples/complete/aws/main.tf
+++ examples/complete/aws/main.tf
@@ -1,6 +1,6 @@
 locals {
   name        = "sonarqube"
-  region      = "us-east-2"
+  region      = "us-east-1"
   environment = "prod"
   additional_tags = {
     Owner      = "organization_name"
@@ -10,9 +10,9 @@ locals {
 }
 
 module "sonarqube" {
-  source           = "https://github.com/sq-ia/terraform-kubernetes-sonarqube.git"
+  source = "../../../"
   sonarqube_config = {
-    hostname                       = "sonarqube.squareops.in"
+    hostname                       = "sonarqube.ldc.squareops.in"
     values_yaml                    = file("./helm/values.yaml")
     storage_class_name             = "gp2"
     sonarqube_volume_size          = "5Gi"
