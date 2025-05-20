7280dc2967403c2376dc821c578bd9457e4f3697
amanravi-squareops <aman.ravi@squareops.comm>
2025-05-20 18:49:04 +0530

    ran pre commit

--- examples/complete/aws/main.tf
+++ examples/complete/aws/main.tf
@@ -1,7 +1,7 @@
 locals {
-  name        = ""
-  region      = ""
-  environment = ""
+  name        = "test"
+  region      = "us-east-1"
+  environment = "dev"
   additional_tags = {
     Owner      = "organization_name"
     Expires    = "Never"
@@ -9,10 +9,11 @@ locals {
   }
 }
 module "sonarqube" {
-  source  = "squareops/sonarqube/kubernetes"
-  version = "3.1.1"
+  # source  = "squareops/sonarqube/kubernetes"
+  # version = "3.1.1"
+  source = "../../../"
   sonarqube_config = {
-    hostname                       = "sonarqube.squareops.in"
+    hostname                       = "sonarqube.test.atmosly.in"
     values_yaml                    = file("./helm/values.yaml")
     storage_class_name             = "gp2"
     sonarqube_volume_size          = "5Gi"
@@ -21,11 +22,11 @@ module "sonarqube" {
     monitoringPasscode             = ""
     postgresql_password_external   = ""
     postgresql_external_server_url = ""
-    sonarqube_password             = "xxxxx"
+    sonarqube_password             = "" # secret must be 15 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character
 
-    updateExistingSonarqube     = false # if you have existing sonarqube and want to upgrade,then enable it.
-    updateExistingSonarqubePassword  = false #if you want to update password,enable it and pass sonarqube_current_password(old),sonarqube_password(new)
-    sonarqube_current_password  = "xxxxxx" # if you upgrade sonarqube then you have to provide your previous sonarqube password ##Secret name=sonarqube-sonarqube-admin-password
-    postgresql_current_password = "xxxxxxx" # if you upgrade sonarqube then you have to provide your previous postgresql password  ##Secret name=sonarqube-postgresql
+    updateExistingSonarqube         = false # if you have existing sonarqube and want to upgrade,then enable it.
+    updateExistingSonarqubePassword = false #if you want to update password,enable it and pass sonarqube_current_password(old),sonarqube_password(new)
+    sonarqube_current_password      = ""    # if you upgrade sonarqube then you have to provide your previous sonarqube password ##Secret name=sonarqube-sonarqube-admin-password
+    postgresql_current_password     = ""    # if you upgrade sonarqube then you have to provide your previous postgresql password  ##Secret name=sonarqube-postgresql
   }
 }
